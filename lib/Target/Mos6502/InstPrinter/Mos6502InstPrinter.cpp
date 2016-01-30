c37ae1e1a98f32ea7b535d956d4ecdc5e718978a
N.E.C <beholdnec@gmail.com>
2016-01-29 21:08:53 -0800

    Print instruction mnemonics

--- lib/Target/Mos6502/InstPrinter/Mos6502InstPrinter.cpp
+++ lib/Target/Mos6502/InstPrinter/Mos6502InstPrinter.cpp
@@ -1,13 +1,34 @@
 // TODO: header stuff
 
 #include "Mos6502InstPrinter.h"
+#include "llvm/MC/MCExpr.h"
+#include "llvm/MC/MCInst.h"
 #include "llvm/Support/raw_ostream.h"
 using namespace llvm;
+
+#include "Mos6502GenAsmWriter.inc"
 
 void Mos6502InstPrinter::printInst(const MCInst *MI, raw_ostream &O,
                                    StringRef Annot,
                                    const MCSubtargetInfo &STI) {
 
-  // TODO
-  O << "garblefart!\n";
+  // print instruction with auto-generated code from the .td files
+  printInstruction(MI, O);
+  printAnnotation(O, Annot);
+}
+
+void Mos6502InstPrinter::printOperand(const MCInst *MI, unsigned OpNo,
+                                      raw_ostream &O) {
+#if 0
+  const MCOperand &Op = MI->getOperand(OpNo);
+  if (Op.isReg()) {
+    O << getRegisterName(Op.getReg());
+  } else if (Op.isImm()) {
+    O << (int)Op.getImm();
+  } else {
+    Op.getExpr()->print(O, &MAI);
+  }
+#else
+  O << "TODO";
+#endif
 }
