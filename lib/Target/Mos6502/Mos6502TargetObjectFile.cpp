09252b613e76d375f89a49dd95d5cf13979bd3ba
N.E.C <beholdnec@gmail.com>
2016-01-29 19:36:45 -0800

    llc now runs without crashing

--- lib/Target/Mos6502/Mos6502TargetObjectFile.cpp
+++ lib/Target/Mos6502/Mos6502TargetObjectFile.cpp
@@ -3,20 +3,3 @@
 #include "Mos6502TargetObjectFile.h"
 
 using namespace llvm;
-
-MCSection *
-Mos6502TargetObjectFile::getExplicitSectionGlobal(const GlobalValue *GV,
-                                                SectionKind Kind,
-                                                Mangler &Mang,
-                                                const TargetMachine &TM) const {
-  // TODO
-  return nullptr;
-}
-
-MCSection *
-Mos6502TargetObjectFile::SelectSectionForGlobal(const GlobalValue *GV,
-                                                SectionKind Kind, Mangler &Mang,
-                                                const TargetMachine &TM) const {
-  // TODO
-  return nullptr;
-}
