5257ca8344de497229731b5426358b8505613207
N.E.C <beholdnec@gmail.com>
2016-02-05 20:03:12 -0800

    Use fake 6502 instructions which take reg operands. Fixes compiling certain input.

--- lib/Target/Mos6502/InstPrinter/Mos6502InstPrinter.cpp
+++ lib/Target/Mos6502/InstPrinter/Mos6502InstPrinter.cpp
@@ -4,6 +4,7 @@
 #include "llvm/MC/MCExpr.h"
 #include "llvm/MC/MCInst.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/ErrorHandling.h"
 using namespace llvm;
 
 #include "Mos6502GenAsmWriter.inc"
@@ -22,8 +23,9 @@ void Mos6502InstPrinter::printOperand(const MCInst *MI, unsigned OpNo,
   const MCOperand &Op = MI->getOperand(OpNo);
   if (Op.isImm()) {
     O << Op.getImm();
+  } else if (Op.isReg()) {
+    O << getRegisterName(Op.getReg());
   } else {
-    // TODO: print other types of operands
-    O << "*TODO*";
+    llvm_unreachable("Invalid assembly operand");
   }
 }
