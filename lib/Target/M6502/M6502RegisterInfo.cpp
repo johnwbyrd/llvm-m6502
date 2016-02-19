cdb407161bf3f5a3a34a26fd9075fbc8a92c11f5
N.E.C <beholdnec@gmail.com>
2016-02-18 17:59:05 -0800

    Emit correct stack offset in stack_pseudo instructions

--- lib/Target/M6502/M6502RegisterInfo.cpp
+++ lib/Target/M6502/M6502RegisterInfo.cpp
@@ -3,6 +3,7 @@
 #include "M6502RegisterInfo.h"
 #include "M6502.h"
 #include "M6502Subtarget.h"
+#include "llvm/CodeGen/MachineFrameInfo.h"
 #include "llvm/CodeGen/MachineFunction.h"
 
 using namespace llvm;
@@ -45,10 +46,11 @@ M6502RegisterInfo::eliminateFrameIndex(MachineBasicBlock::iterator II,
   MachineInstr &MI = *II;
   MachineBasicBlock &MBB = *MI.getParent();
   MachineFunction &MF = *MBB.getParent();
+  MachineFrameInfo *FrameInfo = MF.getFrameInfo();
   const M6502FrameLowering *TFI = getFrameLowering(MF);
+  int Index = MI.getOperand(FIOperandNum).getIndex();
 
-  // TODO
-  MI.getOperand(FIOperandNum).ChangeToImmediate(FIOperandNum);
+  MI.getOperand(FIOperandNum).ChangeToImmediate(FrameInfo->getObjectOffset(Index));
 }
 
 unsigned
